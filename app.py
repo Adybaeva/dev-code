84cad980d876e9964028697574931960eb1d18b0
Ernest Ramirez <ernest@ernestramirez.tech>
2024-11-15 20:37:17 -0600

    add a new message to our python script

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from Python Flask!  Build my image"
+    return "Hello from Python Flask!  This is a new copy of our container!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
