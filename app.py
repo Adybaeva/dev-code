5335662780f184353892bd1635dd7210330a467a
Tansuluu Adybaeva <tansuluu.adybaeva.alt@gmail.com>
2025-03-19 16:30:44 -0500

    building again

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from Python Flask! Third attempt!"
+    return "Hello from Python! Third attempt!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
