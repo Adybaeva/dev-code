78f62810a8b8dd52269540f88f608c67f7309039
Tansuluu Adybaeva <tansuluu.adybaeva.alt@gmail.com>
2025-03-19 14:54:14 -0500

    changing a message in app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from Python Flask!  This is going to deploy automatically!"
+    return "Hello from Python Flask!  This is my first githubactions!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
